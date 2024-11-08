6afdf36f365415d5ae82433e8854ee32871b26d2
Marco Willy <marcowillyazevedo@gmail.com>
2024-11-08 14:29:15 -0300

    fix: solving home events

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,19 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./home.css";
 import React, { useEffect, useState } from "react";
-import {
-  fetchPublicData,
-  createData,
-  updateData,
-  deleteData,
-} from "../../services/apiService";
+import { fetchPublicData } from "../../services/apiService";
 import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
 function Home() {
-  let [eventos, setEventos] = useState([]);
-  // const [data, setData] = useState([]);
+  const [eventos, setEventos] = useState([]);
+
   useEffect(() => {
     const loadData = async () => {
       try {
@@ -24,206 +19,197 @@ function Home() {
       }
     };
     loadData();
-    console.log(eventos);
   }, []);
 
+  useEffect(() => {
+    console.log(eventos); // Log para ver os eventos carregados
+  }, [eventos]);
+
   const eventoDoDia = eventos.filter(
-    (event) =>
-      new Date(event.dataEvento).toLocaleDateString("pt-BR") ===
-      new Date().toLocaleDateString("pt-BR")
+      (event) =>
+          new Date(event.dataEvento).toLocaleDateString("pt-BR") ===
+          new Date().toLocaleDateString("pt-BR")
   );
+
   const proximosEventos = eventos.filter(
-    (event) =>
-      new Date(event.dataEvento).toLocaleDateString("pt-BR") !==
-      new Date().toLocaleDateString("pt-BR")
+      (event) =>
+          new Date(event.dataEvento).toLocaleDateString("pt-BR") !==
+          new Date().toLocaleDateString("pt-BR")
   );
+
   const eventosLivres = eventos.filter(
-    (event) => event.classificacaoIdade === 0
+      (event) => event.classificacaoIdade === 0
   );
-  const indiceAleatorio = Math.floor(Math.random() * eventoDoDia.length);
+
+  const indiceAleatorio =
+      eventoDoDia.length > 0 ? Math.floor(Math.random() * eventoDoDia.length) : -1;
 
   return (
-    <div className="Home">
-      {/* Evento Destaque */}
-      <section className="container my-5 p-4 bg-light rounded">
-        <div className="d-flex justify-content-around">
-          <div className="col-md-8 event-info">
-            <h2 className="mb-3">Destaque</h2>
-            <h3 className="mb-3">{eventoDoDia[indiceAleatorio]?.nomeEvento}</h3>
-            <p className="text-muted">
-              Dia:
-              {new Date(
-                eventoDoDia[indiceAleatorio]?.dataEvento
-              ).toLocaleDateString("pt-BR", {
-                day: "2-digit",
-                month: "2-digit",
-                year: "numeric",
-              })}
-            </p>
-            <p>
-              {" "}
-              Local: {eventoDoDia[indiceAleatorio]?.endereco?.nomeEspaco}
-            </p>
-            <Button variant="primary">
-              <Link
-                to={`/evento/id/${eventoDoDia[indiceAleatorio]?.id}`}
-                className="nav-link"
-              >
-                Saiba mais
-              </Link>
-            </Button>
+      <div className="Home">
+        {/* Evento Destaque */}
+        <section className="container my-5 p-4 bg-light rounded">
+          <div className="d-flex justify-content-around">
+            <div className="col-md-8 event-info">
+              <h2 className="mb-3">Destaque</h2>
+              {indiceAleatorio >= 0 ? (
+                  <>
+                    <h3 className="mb-3">{eventoDoDia[indiceAleatorio]?.nomeEvento || "Nenhum evento disponível"}</h3>
+                    <p className="text-muted">
+                      Dia:
+                      {new Date(eventoDoDia[indiceAleatorio]?.dataEvento).toLocaleDateString("pt-BR", {
+                        day: "2-digit",
+                        month: "2-digit",
+                        year: "numeric",
+                      })}
+                    </p>
+                    <p>
+                      Local: {eventoDoDia[indiceAleatorio]?.endereco?.nomeEspaco || "Local não disponível"}
+                    </p>
+                    <Button variant="primary">
+                      <Link to={`/evento/id/${eventoDoDia[indiceAleatorio]?.id}`} className="nav-link">
+                        Saiba mais
+                      </Link>
+                    </Button>
+                  </>
+              ) : (
+                  <p>Nenhum evento do dia disponível.</p>
+              )}
+            </div>
+            <div className="col-md-4 d-flex justify-content-center align-items-center event-video-placeholder">
+              <img
+                  src={
+                    eventoDoDia[indiceAleatorio]?.capaEvento
+                        ? `data:image/png;base64,${eventoDoDia[indiceAleatorio].capaEvento}`
+                        : "caminho/para/imagem/padrao.png" // Substitua pelo caminho da sua imagem padrão
+                  }
+                  alt="Capa do Evento"
+                  style={{ maxHeight: "200px", width: "auto" }}
+              />
+            </div>
           </div>
-          <div className="col-md-4 d-flex justify-content-center align-items-center event-video-placeholder">
-            <img
-              variant="top"
-              src={`data:image/png;base64,${eventoDoDia[indiceAleatorio]?.capaEvento}`}
-              alt="Capa do Evento"
-              style={{ maxHeight: "200px", width: "auto" }}
-            />
+        </section>
+
+        {/* Eventos Hoje */}
+        <section className="new-events mx-4">
+          <h3 className="mb-4">Eventos hoje</h3>
+          <div className="d-flex flex-wrap">
+            {eventoDoDia.length > 0 ? (
+                eventoDoDia.map((evento) => (
+                    <div className="col-md-3 mb-4" key={evento.id}>
+                      <Card style={{ width: "18rem" }}>
+                        <Card.Img
+                            variant="top"
+                            src={`data:image/png;base64,${evento.capaEvento || "caminho/para/imagem/padrao.png"}`} // Imagem padrão
+                            alt={evento.nomeEvento || "Evento sem imagem"}
+                        />
+                        <Card.Body>
+                          <Card.Title>{evento.nomeEvento}</Card.Title>
+                          <Card.Text>
+                            <p className="event-date text-muted">
+                              {new Date(evento.dataEvento).toLocaleDateString("pt-BR", {
+                                day: "2-digit",
+                                month: "2-digit",
+                                year: "numeric",
+                              })}
+                            </p>
+                            <p>{evento.endereco?.nomeEspaco || "Local não disponível"}</p>
+                          </Card.Text>
+                          <Button variant="primary">
+                            <Link to={`/evento/id/${evento.id}`} className="nav-link">
+                              Saiba mais
+                            </Link>
+                          </Button>
+                        </Card.Body>
+                      </Card>
+                    </div>
+                ))
+            ) : (
+                <p>Nenhum evento disponível.</p>
+            )}
           </div>
-        </div>
-      </section>
+        </section>
 
-      <section className="new-events mx-4">
-        <h3 className="mb-4">Eventos hoje</h3>
-        <div className="d-flex flex-wrap">
-          {eventoDoDia.length > 0 ? (
-            eventoDoDia.map((evento, index) => (
-              <div className="col-md-3 mb-4" key={evento.id}>
-                <Card style={{ width: "18rem" }}>
-                  <Card.Img
-                    variant="top"
-                    src={`data:image/png;base64,${evento.capaEvento}`}
-                  />
-                  <Card.Body>
-                    <Card.Title>{evento.nomeEvento}</Card.Title>
-                    <Card.Text>
-                      <p className="event-date text-muted">
-                        {new Date(evento.dataEvento).toLocaleDateString(
-                          "pt-BR",
-                          {
-                            day: "2-digit",
-                            month: "2-digit",
-                            year: "numeric",
-                          }
-                        )}
-                      </p>
-                      <p>{evento.endereco.nomeEspaco}</p>
-                    </Card.Text>
-                    <Button variant="primary">
-                      <Link to={`/evento/id/${evento.id}`} className="nav-link">
-                        Saiba mais
-                      </Link>
-                    </Button>
-                  </Card.Body>
-                </Card>
-              </div>
-            ))
-          ) : (
-            <p>Nenhum evento disponível.</p>
-          )}
-        </div>
-      </section>
+        {/* Próximos Eventos */}
+        <section className="new-events mx-4">
+          <h3 className="mb-4">Próximos Eventos</h3>
+          <div className="d-flex flex-wrap">
+            {proximosEventos.length > 0 ? (
+                proximosEventos.map((evento) => (
+                    <div className="col-md-3 mb-4" key={evento.id}>
+                      <Card style={{ width: "18rem" }}>
+                        <Card.Img
+                            variant="top"
+                            src={`data:image/png;base64,${evento.capaEvento || "caminho/para/imagem/padrao.png"}`} // Imagem padrão
+                            alt={evento.nomeEvento || "Evento sem imagem"}
+                        />
+                        <Card.Body>
+                          <Card.Title>{evento.nomeEvento}</Card.Title>
+                          <Card.Text>
+                            <p className="event-date text-muted">
+                              {new Date(evento.dataEvento).toLocaleDateString("pt-BR", {
+                                day: "2-digit",
+                                month: "2-digit",
+                                year: "numeric",
+                              })}
+                            </p>
+                            <p>{evento.endereco?.nomeEspaco || "Local não disponível"}</p>
+                          </Card.Text>
+                          <Button variant="primary">
+                            <Link to={`/evento/id/${evento.id}`} className="nav-link">
+                              Saiba mais
+                            </Link>
+                          </Button>
+                        </Card.Body>
+                      </Card>
+                    </div>
+                ))
+            ) : (
+                <p>Nenhum evento disponível.</p>
+            )}
+          </div>
+        </section>
 
-      <section className="new-events mx-4">
-        <h3 className="mb-4">Proximos Eventos</h3>
-        <div className="d-flex flex-wrap">
-          {proximosEventos.length > 0 ? (
-            proximosEventos.map((evento, index) => (
-              <div className="col-md-3 mb-4" key={evento.id}>
-                <Card style={{ width: "18rem" }}>
-                  <Card.Img
-                    variant="top"
-                    src={`data:image/png;base64,${evento.capaEvento}`}
-                  />
-                  <Card.Body>
-                    <Card.Title>{evento.nomeEvento}</Card.Title>
-                    <Card.Text>
-                      <p className="event-date text-muted">
-                        {new Date(evento.dataEvento).toLocaleDateString(
-                          "pt-BR",
-                          {
-                            day: "2-digit",
-                            month: "2-digit",
-                            year: "numeric",
-                          }
-                        )}
-                      </p>
-                      <p>{evento.endereco.nomeEspaco}</p>
-                    </Card.Text>
-                    <Button variant="primary">
-                      <Link to={`/evento/id/${evento.id}`} className="nav-link">
-                        Saiba mais
-                      </Link>
-                    </Button>
-                  </Card.Body>
-                </Card>
-              </div>
-            ))
-          ) : (
-            <p>Nenhum evento disponível.</p>
-          )}
-        </div>
-      </section>
-
-      <section className="new-events mx-4">
-        <h3 className="mb-4">Eventos para toda familia</h3>
-        <div className="d-flex flex-wrap">
-          {eventosLivres.length > 0 ? (
-            eventosLivres.map((evento, index) => (
-              <div className="col-md-3 mb-4" key={evento.id}>
-                <Card style={{ width: "18rem" }}>
-                  <Card.Img
-                    variant="top"
-                    src={`data:image/png;base64,${evento.capaEvento}`}
-                  />
-                  <Card.Body>
-                    <Card.Title>{evento.nomeEvento}</Card.Title>
-                    <Card.Text>
-                      <p className="event-date text-muted">
-                        {new Date(evento.dataEvento).toLocaleDateString(
-                          "pt-BR",
-                          {
-                            day: "2-digit",
-                            month: "2-digit",
-                            year: "numeric",
-                          }
-                        )}
-                      </p>
-                      <p>{evento.endereco.nomeEspaco}</p>
-                    </Card.Text>
-                    <Button variant="primary">
-                      <Link to={`/evento/id/${evento.id}`} className="nav-link">
-                        Saiba mais
-                      </Link>
-                    </Button>
-                  </Card.Body>
-                </Card>
-              </div>
-            ))
-          ) : (
-            <p>Nenhum evento disponível.</p>
-          )}
-        </div>
-      </section>
-    </div>
+        {/* Eventos para toda a família */}
+        <section className="new-events mx-4">
+          <h3 className="mb-4">Eventos para toda a família</h3>
+          <div className="d-flex flex-wrap">
+            {eventosLivres.length > 0 ? (
+                eventosLivres.map((evento) => (
+                    <div className="col-md-3 mb-4" key={evento.id}>
+                      <Card style={{ width: "18rem" }}>
+                        <Card.Img
+                            variant="top"
+                            src={`data:image/png;base64,${evento.capaEvento || "caminho/para/imagem/padrao.png"}`} // Imagem padrão
+                            alt={evento.nomeEvento || "Evento sem imagem"}
+                        />
+                        <Card.Body>
+                          <Card.Title>{evento.nomeEvento}</Card.Title>
+                          <Card.Text>
+                            <p className="event-date text-muted">
+                              {new Date(evento.dataEvento).toLocaleDateString("pt-BR", {
+                                day: "2-digit",
+                                month: "2-digit",
+                                year: "numeric",
+                              })}
+                            </p>
+                            <p>{evento.endereco?.nomeEspaco || "Local não disponível"}</p>
+                          </Card.Text>
+                          <Button variant="primary">
+                            <Link to={`/evento/id/${evento.id}`} className="nav-link">
+                              Saiba mais
+                            </Link>
+                          </Button>
+                        </Card.Body>
+                      </Card>
+                    </div>
+                ))
+            ) : (
+                <p>Nenhum evento disponível.</p>
+            )}
+          </div>
+        </section>
+      </div>
   );
 }
-
-const Base64Image = ({ base64String }) => {
-  return (
-    <div>
-      {base64String ? (
-        <img
-          src={`data:image/png;base64,${base64String}`}
-          alt="Imagem"
-          style={{ maxWidth: "180px", height: "auto" }}
-        />
-      ) : (
-        <p>Imagem não disponível.</p>
-      )}
-    </div>
-  );
-};
 
 export default Home;
