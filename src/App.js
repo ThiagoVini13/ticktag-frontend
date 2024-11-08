3df850aeeceb52e3cb7fd2c4e8efa010b11808ff
Jônatas Fernandes Ferreira <jjonatasfernandes12@gmail.com>
2024-11-08 10:24:31 -0300

    Adiciona formulário de contato com integração ao emailjs e estilização

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import React from 'react';
-import {Route, Routes} from 'react-router-dom';
-import {AuthProvider} from './context/AuthContext';
+import { Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
 import PrivateRoute from './components/PrivateRoute';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -13,58 +13,57 @@ import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Catalago from './pages/Catalogo/Catalogo';
 import Politica from './pages/Politica/Politica';
-import Contato from './pages/Contato/Contato';
+import Contato from './pages/Contato/Contato'; // Já importado corretamente
 import Evento from './pages/Evento/Evento';
 import CriarEvento from './pages/Evento/CriarEvento';
 import Carrinho from './pages/Carrinho/Carrinho';
-
 
 function App() {
     return (
         <AuthProvider>
             <div className="d-flex flex-column min-vh-100">
-            <Navbar/>
-            <div className="App flex-fill">
-                <Routes>
-                    {/* Rotas públicas */}
-                    <Route path="/login" element={<Login/>}/>
-                    <Route path="/register" element={<Register/>}/>
-                    <Route path="/catalogo" element={<Catalago/>}/>
-                    <Route path="/politica" element={<Politica/>}/>
-                    <Route path="/contato" element={<Contato/>}/>
+                <Navbar />
+                <div className="App flex-fill">
+                    <Routes>
+                        {/* Rotas públicas */}
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/catalogo" element={<Catalago />} />
+                        <Route path="/politica" element={<Politica />} />
+                        <Route path="/contato" element={<Contato />} /> {/* Rota de Contato já configurada */}
 
-                    {/* Rotas privadas */}
-                    <Route
-                        path="/"
-                        element={
-                            <PrivateRoute>
-                                <Home/>
-                            </PrivateRoute>
-                        }
-                    />
-                    <Route
-                        path="/evento/id/:idEvento"
-                        element={<Evento/>}
-                    />
-                    <Route
-                        path="/criar-evento"
-                        element={
-                            <PrivateRoute>
-                                <CriarEvento/>
-                            </PrivateRoute>
-                        }
-                    />
-                    <Route
-                        path="/carrinho"
-                        element={
-                            <PrivateRoute>
-                                <Carrinho/>
-                            </PrivateRoute>
-                        }
-                    />
-                </Routes>
-            </div>
-            <Footer/>
+                        {/* Rotas privadas */}
+                        <Route
+                            path="/"
+                            element={
+                                <PrivateRoute>
+                                    <Home />
+                                </PrivateRoute>
+                            }
+                        />
+                        <Route
+                            path="/evento/id/:idEvento"
+                            element={<Evento />}
+                        />
+                        <Route
+                            path="/criar-evento"
+                            element={
+                                <PrivateRoute>
+                                    <CriarEvento />
+                                </PrivateRoute>
+                            }
+                        />
+                        <Route
+                            path="/carrinho"
+                            element={
+                                <PrivateRoute>
+                                    <Carrinho />
+                                </PrivateRoute>
+                            }
+                        />
+                    </Routes>
+                </div>
+                <Footer />
             </div>
         </AuthProvider>
     );
