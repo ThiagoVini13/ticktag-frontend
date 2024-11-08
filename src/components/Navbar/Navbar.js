2b848b105c98f706ae111dc9e6731b48cfaab31e
Marco Willy <marcowillyazevedo@gmail.com>
2024-11-08 00:12:39 -0300

    feat: added criar evento

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Navbar.css';
-import { Link, useNavigate } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 
 function Navbar() {
   const navigate = useNavigate("/register");
   
-  const handleClick = () => {
+  const handleClickRegistrar = () => {
     navigate("/register");
   }
+
+    const handleClick = () => {
+        navigate("/criar-evento");
+    }
 
   return (
     <header className="Home-header d-flex justify-content-between align-items-center p-3 border">
@@ -26,8 +30,8 @@ function Navbar() {
         </ul>
       </nav>
       <div className="auth-buttons">
-        <button className="btn btn-outline-primary me-2">Entrar</button>
-        <button className="btn btn-primary" onClick={handleClick}>Registrar</button>
+        <button className="btn btn-outline-primary me-2" onClick={handleClick}>Criar Evento</button>
+        <button className="btn btn-primary" onClick={handleClickRegistrar}>Registrar</button>
       </div>
     </header>
   );
